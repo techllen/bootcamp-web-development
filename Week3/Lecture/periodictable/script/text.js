1bc87c992042bc4061403d8dd2bf8a0330f62340
techllen <allenmwita21@gmail.com>
2022-03-21 10:50:42 -0400

    periodic table

---
+++ Week3/Lecture/periodictable/script/text.js
@@ -0,0 +1,53 @@
+//console.log("hello world");
+
+
+//string interpolation
+// var fname = "aloyce";
+// var lname = "kisaro";
+// var fullname = `${fname} ${lname}`;
+// console.log(fullname);
+
+// var comment = "This is ";
+
+// console.log(comment + fullname);
+
+
+// var dog = {
+//     hair:"PerformanceEntry",
+//     color:"black",
+//     agressionLevel:"very angry"
+// }
+
+// console.log(dog);
+
+//loads all everything we can get from a window
+//console.log(window);
+
+//load the DOM
+//console.log(document);
+
+// var number=1;
+// function count() {
+//  number++;
+//  console.log(number);
+// }
+
+// count();
+
+// var num1 = 2
+// console.log(num1+3)
+
+// var num2 = 2
+//         num2 = num2 + 3
+//         console.log(num2)
+
+// function add(num3, num4) {
+//     sum = num3 + num4
+//     console.log(sum)
+//     return sum
+// }
+// add(2,3)
+
+function displayName(elementName){
+    console.log(elementName);
+}
