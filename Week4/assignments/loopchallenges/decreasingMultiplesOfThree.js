f856cbec31308df58e54ed288f624442ba00c18d
techllen <allenmwita21@gmail.com>
2022-03-28 13:55:39 -0400

    arrays progress

---
+++ Week4/assignments/loopchallenges/decreasingMultiplesOfThree.js
@@ -0,0 +1,5 @@
+for(var i = 100;i >=0;i--){
+    if(i%3 == 0){
+        console.log(i);
+    }
+}
---
+++ Week4/assignments/loopchallenges/factorial.js
@@ -0,0 +1,6 @@
+var product = 1;
+
+for(var i = 1;i <= 12;i++){
+    product*=i;
+}
+console.log(product);
