c1229b8716cce318f04eb35a6dfa4de4823b9ba6
techllen <allenmwita21@gmail.com>
2022-04-01 20:16:09 -0400

    bash file to create directory structure

--- Week4/lectures/apiAndAjax/js/script.js
+++ Week4/lectures/apiAndAjax/js/script.js
@@ -1,1 +1,39 @@
-fetch
+var foods = ["gillian","breads","fries","rice","pizza","stiff porridge"];
+var cars = [
+    car = {
+        brand:"Tesla",
+        color:"red",
+        model:"model s",
+        range:"500 miles"
+    }
+    ,
+    car2 = {
+        brand:"Tesla",
+        color:"red",
+        model:"model s",
+        range:"500 miles"
+    }
+];
+
+var car = {
+    brand:"Tesla",
+    color:"red",
+    model:"model s",
+    range:"500 miles"
+};
+
+
+// for (const prop in foods) {
+//     if (car.hasOwnProperty.call(foods, prop)) {
+//         //console.log(prop+" : "+cars[prop]);
+//         console.log(foods[prop]);
+//     }
+// }
+
+// for (const food of foods) {
+//     console.log(food);
+// }
+
+foods.forEach(element => {
+    console.log(element);
+});
